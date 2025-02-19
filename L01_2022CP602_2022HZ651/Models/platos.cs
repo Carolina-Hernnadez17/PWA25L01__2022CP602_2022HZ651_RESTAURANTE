a3d3be8de0d6c17ce818fe819156459c58ac708a
Rebeca Margarita Cruz Polanco <rebeca.cruz@catolica.edu.sv>
2025-02-18 19:41:32 -0600

    Comentario

--- L01_2022CP602_2022HZ651/Models/platos.cs
+++ L01_2022CP602_2022HZ651/Models/platos.cs
@@ -4,6 +4,7 @@ namespace L01_2022CP602_2022HZ651.Models
 {
     public class platos
     {
+        //model
         [Key]
         public int platoId { get; set; }
         public string nombrePlato { get; set; }
